17bdce27a531807a73ceb4cba80209c0232ee682
Aakash Saini <121600651+aakashsaini09@users.noreply.github.com>
2024-06-30 17:42:36 +0530

    Update Allproducts.jsx

--- frontend/src/pages/Allproducts.jsx
+++ frontend/src/pages/Allproducts.jsx
@@ -4,7 +4,7 @@ const Allproducts = () => {
   const [openUploadProduct, setopenUploadProduct] = useState(false)
   return (
     // all products listing
-    <div>
+    <div className='allProducts'>
       <div className="bg-white py-2 px-4 flex justify-between items-center" >
         <h2 className='font-bold text-lg'>All Product</h2>
         <button className='border-2 border-purple-600 text-purple-600 hover:text-white hover:bg-purple-600 py-1 px-3 rounded-md' onClick={()=> setopenUploadProduct(true)}>Upload product</button>
@@ -16,5 +16,4 @@ const Allproducts = () => {
     </div>
   )
 }
-
 export default Allproducts
