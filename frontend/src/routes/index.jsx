355c50c2a811cb7ff02625bf5b5c7813c8c913f0
Aakash Saini <121600651+aakashsaini09@users.noreply.github.com>
2024-07-04 18:10:27 +0530

    Update index.jsx

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -40,7 +40,7 @@ const router= createBrowserRouter([
                     {
                         path : "all-products",
                         element: <AllProudcts/>
-                    \
+                    },
                 ]
             },
         ]
