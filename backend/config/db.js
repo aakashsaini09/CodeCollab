096c3f731fae040fc0adde95a2d02380033274cf
aakashsaini09 <aakashsaini948585@gmail.com>
2024-07-28 13:13:28 +0530

    update

--- backend/config/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-async function connectDB(){
-    try{
-        mongoose.connect(process.env.MONGO_URI)
-    }catch(err){
-        console.log(err)
-    }
-}
-module.exports = connectDB
