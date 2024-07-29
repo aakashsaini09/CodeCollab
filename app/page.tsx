16b883a9c80f56c14deae5dd4010c17c0f376eb4
aakashsaini09 <aakashsaini948585@gmail.com>
2024-07-29 15:01:01 +0530

    empty template

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="w-full h-[100vh] flex justify-center text-center items-center bg-black text-white">
-      Hello World
-    </div>
+  <div>
+    Working
+  </div>
   );
 }
